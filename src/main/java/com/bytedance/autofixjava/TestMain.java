4ba40cd4c8e19dd49a5dd036c04ebb68c7c705df
田浩然 <tianhaoran.horace@bytedance.com>
2024-07-31 16:30:15 +0800

    init

--- src/main/java/com/bytedance/autofixjava/TestMain.java
+++ src/main/java/com/bytedance/autofixjava/TestMain.java
@@ -1,15 +1,11 @@
 package com.bytedance.autofixjava;
 
 import com.bytedance.autofixjava.utils.ArrayGenerator;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Arrays;
 
-@SpringBootApplication
-public class AutofixJavaApplication {
+public class TestMain {
 	public static void main(String[] args) {
-		SpringApplication.run(AutofixJavaApplication.class, args);
 		System.out.println("hello world - autofix - java");
 		int[][] arrays = ArrayGenerator.generateRandomTwoDArray(10, 10, 0, 100);
 		System.out.println("生成的二维数组如下：");
